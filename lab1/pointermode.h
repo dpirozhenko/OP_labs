1b9698ede1ee3cb01fbd47304921a182889c29fa
dpirozhenko <114028864+dpirozhenko@users.noreply.github.com>
2023-03-20 14:00:55 +0200

    Create pointermode.h

---
+++ lab1/pointermode.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+
+#pragma once
+
+char* enter_filename();
+void pointermode(const char*);
+void generate_input(const char*);
+void read_file(const char*);
+int countstrings(const char*);
+void deleteOddStrings(const char*);
+void strngs(const char*, const char*);
