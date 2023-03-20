0107f4f25b834f3f95f24c2171a728d5daa62ef6
dpirozhenko <114028864+dpirozhenko@users.noreply.github.com>
2023-03-20 14:01:52 +0200

    Create streammode.h

---
+++ lab1/streammode.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#pragma once
+
+void generate_data(std::string, int);
+void read_file(std::string);
+int count_odd_words(const std::string&);
+void strings(std::string);
+void streammode(std::string, int);
