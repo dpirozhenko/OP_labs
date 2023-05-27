ceb9da7cdd4f8b33b5a92b5a29999bb40335f5ed
dpirozhenko <114028864+dpirozhenko@users.noreply.github.com>
2023-05-27 16:30:03 +0300

    Create main.cpp

---
+++ lab6/main.cpp
@@ -0,0 +1,14 @@
+#include "HashTable.h"
+#include "functions.h"
+
+int main() {
+    int size;
+
+    std::cout << "Enter size of the hash table: ";
+    std::cin >> size;
+
+
+    type(size);
+
+    return 0;
+}
