3d9187aef905b7b567257791ddb58ce26981f7d8
dpirozhenko <114028864+dpirozhenko@users.noreply.github.com>
2023-06-08 17:26:20 +0300

    Update HsTable.cpp

--- lab6/HsTable.cpp
+++ lab6/HsTable.cpp
@@ -1,8 +1,49 @@
 #include "HsTable.h"
 
+template<typename KeyType, typename ValueType>
+unsigned int HashTable<KeyType, ValueType>::MurmurHash2(const KeyType* key, int len, int seed) {
+    const unsigned int m = 0x5bd1e995;
+    const int r = 24;
+
+    unsigned int h = seed ^ len;
+
+    const unsigned char* data = (const unsigned char*)key;
+
+    while (len >= 4) {
+        unsigned int k = *(unsigned int*)data;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h *= m;
+        h ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    switch (len) {
+    case 3:
+        h ^= data[2] << 16;
+    case 2:
+        h ^= data[1] << 8;
+    case 1:
+        h ^= data[0];
+        h *= m;
+    };
+
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return h;
+}
+
 template <typename KeyType, typename ValueType>
-unsigned int HashTable<KeyType, ValueType>::hashFunction(const KeyType& key) {
-    return hash<KeyType>{}(key) % size;
+int HashTable<KeyType, ValueType>::hashFunction(const KeyType& key) {
+    unsigned int hash = MurmurHash2(&key, sizeof(key), 0);
+    return hash % size;
 }
 
 template <typename KeyType, typename ValueType>
@@ -99,7 +140,7 @@ typename HashTable<KeyType, ValueType>::Iterator HashTable<KeyType, ValueType>::
     return Iterator(*this, size);
 }
 
-// Явна інстанціація класу для підтримки роботи з різними типами даних
+
 template class HashTable<int, int>;  
 template class HashTable<int, double>;
 template class HashTable<int, string>;
